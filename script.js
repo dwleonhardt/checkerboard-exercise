b8ff03448a446415f8ac5b4bf452faf940de13f4
dwleonhardt <dwleonhardt@gmail.com>
2017-04-12 16:12:19 -0600

    i made a thing

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Your JS goes here
+
+for (var i = 1; i < 100; i++) {
+  var body = document.getElementsByTagName("body")[0];
+  var tile = document.createElement("div");
+  tile.style.float = 'left';
+  tile.style.width = '11.1%';
+  tile.style.paddingBottom = '11.1%';
+  body.appendChild(tile);
+  if (i % 2 === 0) {
+    tile.style.backgroundColor = 'black';
+  } else {
+    tile.style.backgroundColor = 'red';
+  }
+}
+// var body = document.getElementsByTagName("body")[0];
+// var tile = document.createElement("div");
+// tile.style.float = 'left';
+// tile.style.backgroundColor = 'black';
+// tile.style.width = '11.1%';
+// tile.style.paddingBottom = '11.1%';
+// body.appendChild(tile);
