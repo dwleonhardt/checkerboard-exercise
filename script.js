30aaa6ee2a4fae3cbfe75ebb7deedf436502d47e
dwleonhardt <dwleonhardt@gmail.com>
2017-04-12 16:16:49 -0600

    added random color

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 for (var i = 1; i < 100; i++) {
   var body = document.getElementsByTagName("body")[0];
@@ -7,15 +15,8 @@ for (var i = 1; i < 100; i++) {
   tile.style.paddingBottom = '11.1%';
   body.appendChild(tile);
   if (i % 2 === 0) {
-    tile.style.backgroundColor = 'black';
+    tile.style.backgroundColor = getRandomColor();
   } else {
-    tile.style.backgroundColor = 'red';
+    tile.style.backgroundColor = getRandomColor();
   }
 }
-// var body = document.getElementsByTagName("body")[0];
-// var tile = document.createElement("div");
-// tile.style.float = 'left';
-// tile.style.backgroundColor = 'black';
-// tile.style.width = '11.1%';
-// tile.style.paddingBottom = '11.1%';
-// body.appendChild(tile);
